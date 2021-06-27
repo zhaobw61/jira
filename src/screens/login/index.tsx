0ee4e18550131c42894dc016f43c6ad2fc31648d
zhaobowen <bowen.zhao@chiefclouds.com>
2021-06-27 16:46:28 +0800

    安装工具

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -15,10 +15,11 @@ export const LoginScreen = () => {
             }
         })
     }
-    const handleSubmit = (event: FormEvent<Element>) => {
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        // const username = (event.currentTarget.elements[0] as HTMLInputElement).value;
-        // const password = (event.currentTarget.elements[1] as HTMLInputElement).value;
+        const username = (event.currentTarget.elements[0] as HTMLInputElement).value;
+        const password = (event.currentTarget.elements[1] as HTMLInputElement).value;
+        login({username, password})
     }
     return <form onSubmit={handleSubmit}>
         <div>
@@ -29,6 +30,6 @@ export const LoginScreen = () => {
             <label htmlFor="username">密码</label>
             <input type="password" id={'password'} />
         </div>
-        <button type={"submit"}>登录</button>
+        <button type={"submit"}>注册</button>
     </form>
 }
