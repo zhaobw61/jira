3c7dab667fed507aff99f13f5c909bddcdb7cab1
zhaobowen <bowen.zhao@chiefclouds.com>
2021-07-09 11:52:35 +0800

    修改按钮的样式

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useAuth } from './context/auth-context'
 import {ProjectListScreen} from './screens/project-list'
+import { Button } from 'antd'
 export const AuthenticatedApp = () => {
     const {logout} = useAuth()
     return <div>
-        <button onClick={logout}>登出</button>
+        <Button onClick={logout} type={"primary"}>登出</Button>
         <ProjectListScreen/>
     </div>
 }
